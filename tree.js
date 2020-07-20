4543e2503a2d0135c595d5372ac62c8c036fd41f
xlrn <ncl.yim@gmail.com>
2020-07-20 09:57:29 -0700

    completed implementation of bst

--- tree.js
+++ tree.js
@@ -53,4 +53,97 @@ class BinarySearchTree {
             }
         }
     }
+
+    removeNode(node, data) {
+        if(node === null) {
+            return null;
+        }
+
+        else if(data < node.data) {
+            node.left = this.removeNode(node.left, key);
+            return node;
+        }
+
+        else if(data > node.data) {
+            node.right = this.removeNode(node.right, key);
+            return node;
+        }
+
+        else {
+            if(node.left === null && node.right === null) {
+                node = null;
+                return node;
+            }
+
+            if(node.left === null) {
+                node = node.right;
+                return node;
+            }
+
+            if(node.right === null) {
+                node = node.left;
+                return node;
+            }
+            
+            // Delete node with 2 children
+            var storage = this.findMinNode(node.right);
+            node.data = storage.data;
+
+            node.right = this.removeNode(node.right, storage.data);
+            return node;
+        }
+
+    }
+
+    findMinNode(node) {
+        if (node.left === null) {
+            return node;
+        } else {
+            return this.findMinNode(node.left);
+        }
+    }
+
+    inorder(node) {
+        if (node !== null) {
+            this.inorder(node.left);
+            console.log(node.data);
+            this.inorder(node.right);
+        }
+    }
+
+    preorder(node) {
+        if (node !== null) {
+            console.log(node.data);
+            this.preorder(node.left);
+            this.preorder(node.right);
+        }
+    }
+
+    postorder(node) {
+        if (node !== null) {
+            this.postorder(node.left);
+            this.postorder(node.right);
+            console.log(node.data);
+        }
+    }
+
+    getRootNode() {
+        return this.node;
+    }
+
+    search(data, node) {
+        if (node === null) {
+            return null;
+        }
+
+        else if (data < node.data) {
+            return this.search(data, node.left);
+        }
+
+        else if (data > node.data) {
+            return this.search(data, node.right);
+        }
+
+        else return node;
+    }
 }
