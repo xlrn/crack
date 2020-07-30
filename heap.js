eb7033a6e1cbe0e7000c65472f0b07b0aa2720a0
xlrn <ncl.yim@gmail.com>
2020-07-30 15:40:15 -0700

    added some stuff to heap

--- heap.js
+++ heap.js
@@ -1,3 +1,17 @@
 // heap is tree-like data-structure that allows fast retrieval of the
 // smallest element in the collection
 
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.property = priority;
+        this.next = null;
+    }
+}
+
+class priorityQueue {
+    constructor() {
+        this.first = null;
+    }
+}
+
