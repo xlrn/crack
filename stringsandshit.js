c000523897ba5eff0b784e67a3be4e9783056183
xlrn <ncl.yim@gmail.com>
2020-08-18 10:46:04 -0700

    added crack solution to first question

--- stringsandshit.js
+++ stringsandshit.js
@@ -10,3 +10,24 @@ function isUnique (string) {
 
 console.log(isUnique("aaaaa"));
 console.log(isUnique("abcd"));
+
+function isUniqueCrack (string) {
+    if (string.length > 128) {
+        return false;
+    }
+
+    var char_set = [];
+    for (i = 0; i < string.length; i++) {
+        var val = string.charAt(i);
+        if (char_set[val]) {
+            return false;
+        } 
+        char_set[val] = true;
+
+    }
+    return true;
+
+}
+
+console.log(isUniqueCrack("aaaaa"));
+console.log(isUniqueCrack("abcd"));
